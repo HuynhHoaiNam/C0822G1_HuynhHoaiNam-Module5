b30a6e5d533737e23b1ca8c9977f002019c89f59
HuynhHoaiNam <hhnam098@gmail.com>
2023-02-06 16:22:46 +0700

    up

---
+++ case_study_main/case-study/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeFuramaComponent } from './component/home-furama/home-furama.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeFuramaComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
